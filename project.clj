4402a322e93e97a2eac7710692d18e63b774e1da
John Walker <john.lou.walker@gmail.com>
2014-09-20 07:59:18 -0700

    Add cljx

--- project.clj
+++ project.clj
@@ -8,13 +8,23 @@
                  [org.clojure/tools.reader "0.8.3"]
                  [fast-zip "0.4.0"]
                  [potemkin "0.3.4"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [joda-time]]
+  
+
+  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.4.0"]
+                                  [midje "1.6.3" :exclusions [joda-time]]
                                   [joda-time "2.3"]]
                    :plugins [[lein-midje "3.1.3"]]
                    :exclusions [org.clojure/clojure]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+  :cljx {:builds [{:source-paths ["src/rewrite_clj"]
+                   :output-path "target/classes"
+                   :rules :clj}
+
+                  {:source-paths ["src/rewrite_clj"]
+                   :output-path "target/classes"
+                   :rules :cljs}]}
   :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6"]
             "test-ancient" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "midje"]}
   :pedantic? :abort)
